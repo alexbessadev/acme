5dd3e6099d3050d427e9c69a78f934e8f5107c49
Alex Bessa <deveducador@gmail.com>
2026-03-02 20:31:40 +0000

    feat: implementar moldura raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+import { inter } from '@/app/ui/fonts';
+import '@/app/globals.css';
+
+export const metadata: Metadata = {
+  title: 'ACME'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+};
