b27be49023288d1fd2036f950ee216610ecf294c
Alex Bessa <deveducador@gmail.com>
2026-03-02 21:21:31 +0000

    chore: configurar framework css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss';
 
-export default {
-  content: [],
+const config: Config = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
   plugins: []
 };
+
+export default config;
