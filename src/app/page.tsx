5356e296586701f0f0635273675b51899c6a45e4
Alex Bessa <deveducador@gmail.com>
2026-03-13 17:12:28 +0000

    fix: corrigir palavra de mensagem temporaria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="h-screen bg-black flex items-center justify-center">
-      <h1 className="text-center text-4xl font-semibold  text-amber-50">Sistema em contrução.</h1>
+      <h1 className="text-center text-4xl font-semibold  text-amber-50">Sistema em construção.</h1>
     </main>
   );
 };
